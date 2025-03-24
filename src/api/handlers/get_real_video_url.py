c3bff1e088fe0808fd59d3d28dbfca7b62852c1d
Fernando Corrales <fscorrales@gmail.com>
2025-03-24 11:00:58 -0300

    add get_real_video_url handler and update dependencies

---
+++ src/api/handlers/get_real_video_url.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+"""
+Author : Fernando Corrales <fscpython@gmail.com>
+Date   : 24-mar-2025
+Purpose: Get the real video url with yt-dlp
+         Sirve para descargar videos o extraer enlaces directos de plataformas
+         como: ✅ YouTube, Twitter, TikTok, Facebook, Instagram, Twitch, Reddit, etc.
+         ✅ Algunos sitios de streaming como Vimeo y Dailymotion.
+         ❌ No funciona con MEGA, Zippyshare o 1Fichier, porque no ofrecen streaming directo.
+"""
+
+__all__ = ["get_real_video_url"]
+
+import argparse
+import subprocess
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description="""Fetches the latest anime releases from AnimeFLV.
+
+            This script retrieves a list of the most recent anime releases
+            from AnimeFLV.
+
+            Example usage:
+                python -m src.api.handlers.get_latest_animes
+        """,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    parser.add_argument(
+        "url",
+        metavar="str",
+        help="A URL to get the real video URL",
+        type=str,
+    )
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def get_real_video_url(url: str) -> str:
+    # Ejecutar yt-dlp dentro del entorno virtual
+    cmd = ["yt-dlp", "-g", url]  # "-g" obtiene solo la URL del video
+    real_url = subprocess.run(cmd, capture_output=True, text=True).stdout.strip()
+
+    return real_url
+
+
+# --------------------------------------------------
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+
+    try:
+        real_url = get_real_video_url(url=args.url)
+        print("Enlace directo:", real_url)
+    except Exception as e:
+        print(e)
+
+
+# --------------------------------------------------
+if __name__ == "__main__":
+    main()
+
+    # python -m src.api.handlers.get_real_video_url "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
